899cc06d0649d06ab44949d5df830598b852989c
GET Academy <terje@kolderup.net>
2024-02-13 15:41:13 +0100

    x

---
+++ DataStructures/SimplifiedDataStructures/TerjesBinaryTree.cs
@@ -0,0 +1,46 @@
+﻿namespace DataStructures.SimplifiedDataStructures
+{
+    internal class TerjesBinaryTree
+    {
+        private TerjesBinaryTreeNode? root;
+
+        public void Add(string key, int value)
+        {
+            root = Add(root, key, value);
+        }
+
+        public TerjesBinaryTreeNode Add(TerjesBinaryTreeNode root, string key, int value)
+        {
+            if (root == null)
+            {
+                root = new TerjesBinaryTreeNode(key, value);
+                return root;
+            }
+
+            int comparison = string.CompareOrdinal(key, root.Key);
+
+            if (comparison < 0)
+                root.Left = Add(root.Left, key, value);
+            else if (comparison > 0)
+                root.Right = Add(root.Right, key, value);
+            else
+                throw new ApplicationException("Duplicate keys not allowed");
+
+            return root;
+        }
+
+        public TerjesBinaryTreeNode Find(string key, TerjesBinaryTreeNode node = null)
+        {
+            if (root == null || root.Key == key) return root;
+            if (node == null) node = root;
+
+
+            int comparison = string.CompareOrdinal(key, root.Key);
+
+            if (comparison < 0)
+                return Find(key, root.Left);
+            else
+                return Find(key, root.Right);
+        }
+    }
+}
---
+++ DataStructures/SimplifiedDataStructures/TerjesBinaryTreeNode.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructures.SimplifiedDataStructures
+{
+    internal class TerjesBinaryTreeNode
+    {
+        public string Key { get; set; }
+        public int Value { get; set; }
+        public TerjesBinaryTreeNode Left { get; set; }
+        public TerjesBinaryTreeNode Right { get; set; }
+
+        public TerjesBinaryTreeNode(string key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
