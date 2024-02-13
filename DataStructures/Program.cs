966ed22aa46784db690310e4525d5e2aaaf67582
GET Academy <terje@kolderup.net>
2024-02-13 15:44:01 +0100

    Update Program.cs

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,6 +1,11 @@
 ﻿
 // C#-klasser:
 var list = new List<string>();
+var sortedList = new SortedList<string,int>();
 var linkedList = new LinkedList<string>();
 var dictionary = new Dictionary<string,int>();
 var sortedDictionary = new SortedDictionary<string,int>();
+var hashSet = new HashSet<string>();
+var sortedSet = new SortedSet<string>();
+var stack = new Stack<string>();
+var queue = new Queue<string>();
